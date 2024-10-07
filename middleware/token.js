c532a662abd6ce75dfbba6b29db072d0bca562e4
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-10-07 06:21:40 +0100

    update token

--- middleware/token.js
+++ middleware/token.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
-
+    console.log(token)
   if (!token) return res.status(403).send("A token is required for authentication");
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
